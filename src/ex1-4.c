e1bafdb42a3e9c3c414d9205a8fac1f5fac001a3
Frank <francisco.licea@gmail.com>
2012-08-21 23:56:29 -0500

    Add ex1-4

---
+++ src/ex1-4.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <assert.h>
+#include <debug.h>
+
+int sort_fn(const void *a, const void *b)
+{
+	return (*(char*)a) - (*(char*)b);
+}
+
+int is_anagram(char *a, int a_len, char *b, int b_len)
+{
+	assert(NULL != a && a_len >= 0 && NULL != b && b_len >= 0 && "Invalid args");
+
+	if(a_len != b_len) return 0;
+
+	char *a_cpy = malloc((a_len + 1) * sizeof(char));
+	check_mem(a_cpy);
+
+	char *b_cpy = malloc((b_len + 1) * sizeof(char));
+	check_mem(b_cpy);
+
+	strncpy(a_cpy, a, a_len);
+	strncpy(b_cpy, b, b_len);
+
+	qsort(a_cpy, a_len, sizeof(char), sort_fn);
+	qsort(b_cpy, b_len, sizeof(char), sort_fn);
+
+	int is_anagram = (0 == strncmp(a_cpy, b_cpy, a_len));
+
+	free(a_cpy);
+	free(b_cpy);
+
+	return is_anagram;
+	
+error:
+
+	free(a_cpy);
+	free(b_cpy);
+
+	return -1;
+}
---
+++ tests/ex1-4_tests.c
@@ -0,0 +1,64 @@
+#include <ex1-4.c>
+#include "minunit.h"
+
+char *empty_str()
+{
+	char a[] = "";
+	char b[] = "";
+	mu_assert(1 == is_anagram(a, sizeof(a) - 1,
+				              b, sizeof(b) - 1),
+			  "Failed with empty string");
+
+	return NULL;
+}
+
+char *str_len_1()
+{
+	char a[] = "a";
+	char b[] = "a";
+
+	mu_assert(1 == is_anagram(a, sizeof(a) - 1,
+				              b, sizeof(b) - 1),
+			  "Failed with str len 1");
+
+	return NULL;
+}
+
+char *anagram()
+{
+	char a[] = "unnoticed";
+	char b[] = "continued";
+
+	mu_assert(1 == is_anagram(a, sizeof(a) - 1,
+				              b, sizeof(b) - 1),
+			  "Failed with anagram");
+
+	return NULL;
+}
+
+char *not_anagram()
+{
+	char a[] = "unnoticed";
+	char b[] = "ravenholm";
+
+	mu_assert(0 == is_anagram(a, sizeof(a) - 1,
+				              b, sizeof(b) - 1),
+			  "Failed with str that is not an anagram");
+
+	return NULL;
+
+}
+
+char *all_tests()
+{
+	mu_suite_start();
+
+	mu_run_test(empty_str);
+	mu_run_test(str_len_1);
+	mu_run_test(anagram);
+	mu_run_test(not_anagram);
+
+	return NULL;
+}
+
+RUN_TESTS(all_tests);
